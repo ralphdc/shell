61218ba98cb83d57a1849a2b72c7526aceb05a6b
ralphdc <472298551@qq.com>
2019-03-18 02:52:17 +0800

    new contents

--- ipset.sh
+++ ipset.sh
@@ -20,6 +20,17 @@ do
 	fi
 done
 
+
+
+for ip in `journalctl | grep -E -o "Failed password for invalid user (.*) from (.*) port"  | awk '{print $8}' | sort -r | uniq`
+do 
+	if [ $(ipset list | grep "$ip" | wc -l) -lt 1 ];then
+		ipset add banips "$ip/24" > /tmp/ipset.log 2>&1 
+	fi
+done
+
+
+
 if [ $(iptables -L -n | grep banips | wc -l) -lt 1 ];then
 	iptables -I INPUT -m set --match-set banips src -p TCP -j DROP
 fi 
